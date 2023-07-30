60ba12b4231b83a33cf99f26d2e1fdd13fcf0e14
Cookie Collie <ediblechocolatething@gmail.com>
2023-07-31 03:01:50 +0700

    fix: Fix private key

--- googleSheetLogic.js
+++ googleSheetLogic.js
@@ -2,7 +2,7 @@ const google = require("googleapis")
 require("dotenv").config()
 
 const CLIENT_EMAIL = process.env.CLIENT_EMAIL
-const PRIVATE_KEY = process.env.PRIVATE_KEY
+const PRIVATE_KEY = process.env.PRIVATE_KEY.replace(/\\n/g, '\n')
 const SHEET_ID = process.env.SHEET_ID
 
 const client = new google.Auth.JWT(CLIENT_EMAIL, null, PRIVATE_KEY, ["https://www.googleapis.com/auth/spreadsheets"])
