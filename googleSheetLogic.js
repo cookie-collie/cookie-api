0a27a45ed67198ed5b3031a0edfaf32c50a36545
Cookie Collie <ediblechocolatething@gmail.com>
2023-07-31 18:17:31 +0700

    fix: Fix JWT can't read private key (pt.2)

--- googleSheetLogic.js
+++ googleSheetLogic.js
@@ -5,10 +5,15 @@ const CLIENT_EMAIL = process.env.CLIENT_EMAIL
 const PRIVATE_KEY = process.env.PRIVATE_KEY.replace(/\\n/g, '\n')
 const SHEET_ID = process.env.SHEET_ID
 
+// const b64_PRIVATE_KEY = Buffer.from(PRIVATE_KEY_RAW).toString("base64")
+// const PRIVATE_KEY = `
+// -----BEGIN PRIVATE KEY-----
+// ${b64_PRIVATE_KEY}
+// -----END PRIVATE KEY-----
+// `
+
 const client = new google.Auth.JWT(CLIENT_EMAIL, null, PRIVATE_KEY, ["https://www.googleapis.com/auth/spreadsheets"])
 
 const sheet = google.google.sheets({version: "v4", auth: client})
-
-client.getAccessToken().then(res=>console.log(res))
 
 module.exports = sheet
